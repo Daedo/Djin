f863cf0fd7daf31fdfbbaad6627887918da45748
Daedo <daedo@daedo-play.de>
2016-04-14 20:25:27 +0200

    Added ArgParser

---
+++ Parser/Daedalus Interpreter/daedalusExecution/exception/DaedalusIllegalVarAccessException.java
@@ -0,0 +1,14 @@
+package daedalusExecution.exception;
+
+public class DaedalusIllegalVarAccessException extends DaedalusException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DaedalusIllegalVarAccessException(String message) {
+		super(message);
+	}
+
+}
---
+++ Parser/Djin GUI/gui/ArgParser.java
@@ -0,0 +1,9 @@
+package gui;
+
+public class ArgParser {
+
+	public static String[] parse(String text) {
+		return text.split(" ");
+	}
+
+}
